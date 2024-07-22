41a28c916336540deb1a1831c5613bed59a5ae0a
Leonardo Bertoletti <Leonardo.Bertoletti@edu.pucrs.br>
2024-07-22 12:16:32 -0300

    start

---
+++ app/index.tsx
@@ -0,0 +1,21 @@
+import { Text, View,StyleSheet,Image } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Ted</Text>
+    
+    </View>
+  );
+  const styles = StyleSheet.create({
+    image: {
+
+    }
+  })
+}
